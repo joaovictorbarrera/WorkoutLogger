bc449141e29adae7b7087c939d7200a60809afaf
joaovictorbarrera <joao.victor.lotfi@gmail.com>
2025-10-09 00:45:49 -0400

    Wrote documentation on Readme and removed it from CLIApp

--- src/main/java/org/joaobarrera/CLIApp.java
+++ src/main/java/org/joaobarrera/CLIApp.java
@@ -5,25 +5,6 @@ import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Scanner;
 
-/*
-==File Import Documentation==
-The final product will have a website in which the WorkoutIDs will be invisible to the user.
-The records will have an ID behind the scenes, but the user will not know about the ID visually.
-An ID will be automatically generated for a record when it's added either through a file import or adding manually.
-
-So, file imports do not expect WorkoutID. They only expect the 6 points of data:
-name, startDateTime, duration, distance, unit, notes
-
-Here is an example:
-
-Morning Run,2025-10-07T06:30,45,5.0,KILOMETERS,Felt great
-Evening Walk,2025-10-07T18:15,30,2.0,MILES,Relaxing walk
-Lunch Jog,2025-10-07T12:00,25,3.5,KILOMETERS,Quick jog before work
-
-The program will only export to .txt and .csv file paths, but it can read any kind of text-based file.
-
-*/
-
 class CLIApp {
     private final WorkoutManager workoutManager;
     private final Scanner scanner;
